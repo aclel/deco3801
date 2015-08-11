299d100603a386b6006f0eff97e597081dd623a7
Andrew Cleland <andrew.cleland3@gmail.com>
2015-08-11 19:15:44 +1000

    Change routes to use /api/v1/ endpoint

--- server/router.go
+++ server/router.go
@@ -16,11 +16,11 @@ import (
 func NewRouter(env *config.Env) *mux.Router {
 	r := mux.NewRouter().StrictSlash(true)
 	// Authenticated routes
-	r.Handle("/buoys", AuthHandler{env, handlers.BuoysIndex})
+	r.Handle("/api/v1/buoys", AuthHandler{env, handlers.BuoysIndex})
 
 	// Unauthenticated routes
-	r.Handle("/users", AppHandler{env, handlers.UsersCreate})
-	r.Handle("/login", AppHandler{env, handlers.Login})
+	r.Handle("/api/v1/users", AppHandler{env, handlers.UsersCreate})
+	r.Handle("/api/v1/login", AppHandler{env, handlers.Login})
 
 	return r
 }

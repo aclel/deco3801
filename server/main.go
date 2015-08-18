23ff3602f1721f84a4f109da8254a6a39a87aae2
Andrew Cleland <andrew.cleland3@gmail.com>
2015-08-18 22:03:34 +1000

    Construct db connection string from ENV variables
    - Environment variables now need to be defined to be able to connect
      to the database:
    
    FMS_DB_HOST="localhost"
    FMS_DB_PORT="3306"
    FMS_DB_USERNAME="deco3801"
    FMS_DB_PASSWORD="deco3801"
    FMS_DB_NAME="deco3801"
    
    The values are just shown as an example

--- server/main.go
+++ server/main.go
@@ -5,10 +5,32 @@ import (
 	"net/http"
 
 	"github.com/aclel/deco3801/server/config"
+	"github.com/kelseyhightower/envconfig"
 )
 
+type Config struct {
+	DbHost     string `envconfig:"db_host"`
+	DbPort     string `envconfig:"db_port"`
+	DbUsername string `envconfig:"db_username"`
+	DbPassword string `envconfig:"db_password"`
+	DbName     string `envconfig:"db_name"`
+}
+
 func main() {
-	db, err := config.NewDB("deco3801:deco3801@tcp(localhost:3306)/deco3801")
+	var conf Config
+	err := envconfig.Process("fms", &conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dataSourceName := conf.DbUsername + ":" +
+		conf.DbPassword + "@tcp(" +
+		conf.DbHost + ":" +
+		conf.DbPort + ")/" +
+		conf.DbName
+
+	db, err := config.NewDB(dataSourceName)
+
 	if err != nil {
 		log.Println("Connection to database was unsuccessful.")
 		log.Panic(err)
@@ -16,7 +38,7 @@ func main() {
 	env := &config.Env{DB: db}
 	router := NewRouter(env)
 
-	log.Println("Database connection successful. Connected to localhost:3306/deco3801")
+	log.Println("Database connection successful. Connected to " + dataSourceName)
 	log.Println("Web service listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }

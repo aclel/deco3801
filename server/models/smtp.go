933bc208c6099f49514aac7569629930b3b64a9b
Andrew Cleland <andrew.cleland3@gmail.com>
2015-08-30 01:54:23 +1000

    Remove hardcoded email recipient
    - Email recipient is now the actual recipient
    
    - Added "secret123" temp password to email template

--- server/models/smtp.go
+++ server/models/smtp.go
@@ -16,12 +16,13 @@ type EmailCredentials struct {
 func SendNewUserEmail(user *User, emailUser *EmailCredentials) error {
 	m := gomail.NewMessage()
 	m.SetHeader("From", emailUser.Username)
-	m.SetHeader("To", "andrew.cleland@uqconnect.edu.au")
+	m.SetHeader("To", user.Email)
 	m.SetHeader("Subject", "UQ Flood Monitoring System Account")
 	m.SetBody("text/html",
 		`<h1>Welcome to the UQ Flood Monitoring System.</h1>
 		<p>Here is a link to sign in to your account: </p>
 		<a href="http://teamneptune.co">Sign In</a>
+		<p>You temporary password is "secret123"</p>
 		<p>You will be prompted to change your password when you sign in for the first time.</p>`)
 
 	d := gomail.NewPlainDialer(emailUser.Server, emailUser.Port, emailUser.Username, emailUser.Password)
@@ -30,7 +31,7 @@ func SendNewUserEmail(user *User, emailUser *EmailCredentials) error {
 		return err
 	}
 
-	log.Println("Signup email sent to andrew.cleland@uqconnect.edu.au")
+	log.Println("Signup email sent to " + user.Email)
 
 	return nil
 }

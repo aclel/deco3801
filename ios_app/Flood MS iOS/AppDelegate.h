14ca575c0e4d7600cb21dedb907534765c101a16
Sean M <seanm@hjs.local>
2015-09-03 20:17:06 +1000

    Initial commit for ios app. I've never used git before; be gentle.

---
+++ ios_app/Flood MS iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flood MS iOS
+//
+//  Created by Sean M on 3/09/2015.
+//  Copyright (c) 2015 Team Neptune. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios_app/Flood MS iOS/LoginScreen.m
@@ -0,0 +1,27 @@
+//
+//  LoginScreen.m
+//  Flood MS iOS
+//
+//  Created by Sean M on 3/09/2015.
+//  Copyright (c) 2015 Team Neptune. All rights reserved.
+//
+
+#import "LoginScreen.h"
+
+@interface LoginScreen ()
+
+@end
+
+@implementation LoginScreen
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

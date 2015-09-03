f205acb7a3a166609867c10823878122fbeefe9f
henrycjc <henry.ponco@gmail.com>
2015-09-04 02:40:09 +1000

    Added scripts for adding environment variables

---
+++ env.bat
@@ -0,0 +1,12 @@
+:: Note, getx only works on >Win7. For Vista and earlier install Windows Resource Kit
+getx FMS_DB_HOST localhost
+getx FMS_DB_PORT 3306
+getx FMS_DB_USERNAME <username>   :: Remember to replace this.
+getx FMS_DB_PASSWORD <password>   :: Remember to replace this.
+getx FMS_DB_NAME deco3801         :: Remember to replace this as necessary.
+getx FMS_SMTP_USERNAME <username> :: Ask Andrew C for this.
+getx FMS_SMTP_PASSWORD <password> :: Ask Andrew C for this.
+getx FMS_SMTP_SERVER smtp.gmail.com
+getx FMS_SMTP_PORT 587
+getx FMS_PRIVATE_KEY <key>        :: Remember to replace this.
+getx FMS_PUBLIC_KEY <key>         :: Remember to replace this.

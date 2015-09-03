24a840a36450bb749d7a5933742f7cdc540c0923
Sean M <seanm@hjs.local>
2015-09-03 19:53:10 +1000

    Initial Commit

---
+++ Flood MS iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flood MS iOS
+//
+//  Created by Sean M on 3/09/2015.
+//  Copyright (c) 2015 Team Neptune. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Flood MS iOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Flood MS iOS
+//
+//  Created by Sean M on 3/09/2015.
+//  Copyright (c) 2015 Team Neptune. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

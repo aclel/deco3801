1d50a9f39aee42f421a17677a1aec2c4226f65ac
Andrew Cleland <andrew.cleland3@gmail.com>
2015-09-06 00:35:04 +1000

    Change /api/readings incoming JSON format
    - The incoming JSON format has been changed to remove redundancy.
      See buoy_reading.go for an example of the new JSON.
    
    - An array of readings can now be sent in one request to the route
      and all of them will be inserted into the database.
    
    - The Reading timestamp is currently a string. It will be changed
      to time.Time in the near future.
    
    - All reading tests have been updated. More tests need to be added
      to test the readings array properly.

---
+++ server/models/buoy_reading.go
@@ -0,0 +1,49 @@
+package models
+
+// These structs are needed to store the JSON from POST /api/readings.
+// The data from these structs is then used to build a Readings object
+// for each individual sensor reading. Below is an example of the
+// incoming JSON:
+//{
+//	"guid":"e7ed6c08-d969-4907-b855-bb04ce32c28a",
+//	"readings": [
+//		{
+//	    	"latitude": -27.425676,
+//	    	"longitude": 153.147055,
+//	    	"sensorReadings": [
+//				{
+//	        		"sensorName": "Turbidity",
+//	        		"value": 40
+//	    		}
+//			],
+//	    	"timestamp": "2006-01-02T15:04:05Z07:00",
+//	    	"messageNumber": 1
+//		}
+//	]
+//}
+
+// Encapsulates the readings in the incoming /api/readings/
+// POST request. Every reading will have the same buoy guid
+// so there's no point in repeating it.
+type BuoyReadingContainer struct {
+	BuoyGuid string        `json:"guid"`
+	Readings []BuoyReading `json:"readings"`
+}
+
+// Stores the data for multiple sensor readings.
+// Each sensor reading within a buoy reading has the same latitude
+// and longitude and timestamp, so there's no point in repeating it.
+type BuoyReading struct {
+	Latitude       float64         `json:"latitude"`
+	Longitude      float64         `json:"longitude"`
+	SensorReadings []SensorReading `json:"sensorReadings"`
+	Timestamp      string          `json:"timestamp"`
+	MessageNumber  int             `json:"messageNumber"`
+}
+
+// Stores the name of a sensor and the value associated with
+// a particular reading for that sensor.
+type SensorReading struct {
+	SensorName string  `json:"sensorName"`
+	Value      float64 `json:"value"`
+}

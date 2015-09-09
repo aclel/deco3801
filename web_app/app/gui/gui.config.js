fff1fd8c52d1773ebb58c9b73f72fa35addbce5b
Andrew Dyer <andrew@dyergroup.com.au>
2015-09-09 21:43:56 +1000

    Move timepicker config into app.gui

--- web_app/app/gui/gui.config.js
+++ web_app/app/gui/gui.config.js
@@ -1,7 +1,7 @@
 (function() {
 	'use strict';
 	
-	angular.module('app')
+	angular.module('app.gui')
 		.config(function($datepickerProvider, $timepickerProvider, $httpProvider) {
 			
 			$httpProvider.interceptors.push('authInterceptor');

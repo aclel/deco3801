2b4596871439dcdcd3391cd9c76a383f6f243cbc
Andrew Dyer <andrew@dyergroup.com.au>
2015-09-12 04:00:09 +1000

    Fix for routing digest loop on startup?

--- web_app/app/auth/auth.config.js
+++ web_app/app/auth/auth.config.js
@@ -1,27 +1,27 @@
-// (function() {
-// 	'use strict';
+(function() {
+	'use strict';
 	
-// 	angular.module('app.auth')
-// 		.run(authRoute)
+	angular.module('app.auth')
+		.run(authRoute)
 		
-// 	function authRoute($rootScope, $state, auth) {
-// 		$rootScope.$on('$stateChangeStart', stateChange);
+	function authRoute($rootScope, $state, auth) {
+		$rootScope.$on('$stateChangeStart', stateChange);
 		
-// 		function stateChange(event, toState, toParams,
-// 				fromState, fromParams) {
+		function stateChange(event, toState, toParams,
+				fromState, fromParams) {
 				
-// 			if (!auth.checkUser(toState.data.access)) {
-// 				event.preventDefault();
+			if (!auth.checkUser(toState.data.access)) {
+				event.preventDefault();
 				
-// 				// only redirect if page was just loaded
-// 				if (fromState.url === '^') {
-// 					if (auth.loggedIn()) {
-// 						$state.go('dashboard');
-// 					} else {
-// 						$state.go('login');
-// 					}
-// 				}
-// 			}			
-// 		}
-// 	}
-// })();
+				// only redirect if page was just loaded
+				if (fromState.url === '^') {
+					if (auth.loggedIn()) {
+						$state.go('dashboard');
+					} else {
+						$state.go('login');
+					}
+				}
+			}			
+		}
+	}
+})();

73dd6315d7f812f5c1db7806f6c2313cc94955c6
Andrew Dyer <andrew@dyergroup.com.au>
2015-10-01 01:17:16 +1000

    Add placeholders to Add User input fields

--- web_app/app/admin/admin.controller.js
+++ web_app/app/admin/admin.controller.js
@@ -144,7 +144,7 @@
 		 * called on Add User button click
 		 */
 		function startAddingUser() {
-			var tempUser = { id: -2 };
+			var tempUser = { id: -2, role: vm.roles[0] };
 			vm.users.push(tempUser);
 			startEditingUser(tempUser);
 		}

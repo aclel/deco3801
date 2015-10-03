b12cfcf1cc87fad4a75402a988b09630878956a9
Andrew Cleland <andrew.cleland3@gmail.com>
2015-10-04 00:33:52 +1000

    Revert "Delete handler tests"
    This reverts commit 74afdec6282c1b502d6d1259cf31ea228a29f8cc.

---
+++ server/handlers/user_test.go
@@ -0,0 +1,53 @@
+// Flood Monitoring System
+// Version 0.0.1 (Duyung)
+//
+// Copyright (C) Team Neptune
+// All rights reserved.
+//
+// @author     Andrew Cleland <andrew.cleland3@gmail.com>
+// @version    0.0.1
+// @copyright  Team Neptune (2015)
+// @link       https://github.com/aclel/deco3801
+package handlers
+
+/*
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/aclel/deco3801/server/models"
+)
+
+var userCreateTests = []struct {
+	in  string
+	out int
+}{
+	{`{"email":"test@email.com"}`, http.StatusCreated},
+	{`{"email":""}`, http.StatusBadRequest},
+	{`{"email":"test@"}`, http.StatusBadRequest},
+	{`dfgdfg`, http.StatusBadRequest},
+	{`{}`, http.StatusBadRequest},
+}
+
+func TestUsersCreate(t *testing.T) {
+	env := &models.Env{DB: &models.MockDB{}}
+
+	for _, tt := range userCreateTests {
+		rec := httptest.NewRecorder()
+		reqBody := []byte(tt.in)
+		req, err := http.NewRequest("POST", "/api/users", bytes.NewBuffer(reqBody))
+		req.Header.Set("Content-Type", "application/json")
+		if err != nil {
+			t.Error(err)
+		}
+
+		AppHandler{env, UsersCreate}.ServeHTTP(rec, req)
+
+		if rec.Code != tt.out {
+			t.Errorf("%v: HTTP status = %v, want %v", tt.in, rec.Code, tt.out)
+		}
+	}
+}
+*/

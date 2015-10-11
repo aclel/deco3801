4280ad1f3110653ba3c14152e9249d2cb1b7636c
Andrew Cleland <andrew.cleland3@gmail.com>
2015-10-12 02:10:50 +1000

    Elevate permissions on build script

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Build and run data volume and mysql
-docker-compose -f databases-compose.yml build
-docker-compose -f databases-compose.yml up -d
+sudo docker-compose -f databases-compose.yml build
+sudo docker-compose -f databases-compose.yml up -d
 
 sleep 10
 
 # Build and run goserver, phpmyadmin and nginx
-docker-compose build
-docker-compose up -d
+sudo docker-compose build
+sudo docker-compose up -d
 

0b7ed7331e5c3b2dd656002fafa36260dc385d3f
Andrew Dyer <andrew@dyergroup.com.au>
2015-10-17 01:44:32 +1000

    Add element focusing functionality

---
+++ web_app/app/gui/focusOn.directive.js
@@ -0,0 +1,28 @@
+/**
+ * Flood Monitoring System
+ * Version 0.0.1 (Duyung)
+ *
+ * Copyright (C) Team Neptune
+ * All rights reserved.
+ *
+ * @author     Andrew Dyer <andrew@dyergroup.com.au>
+ * @version    0.0.1
+ * @copyright  Team Neptune (2015)
+ * @link       https://github.com/aclel/deco3801
+ */
+(function() {
+    'use strict';
+    
+    angular.module('app.gui')
+        .directive('focusOn', focusOn);
+    
+    function focusOn() {
+        return function(scope, elem, attr) {
+            scope.$on('focusOn', function(e, name) {
+                if (name === attr.focusOn) {
+                    elem[0].focus();
+                }
+            });
+        }
+    }
+})();

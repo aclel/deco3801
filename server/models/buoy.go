e5dab5b6280a52caf1a895131c3d3ab3f6be8af2
Andrew Cleland <andrew.cleland3@gmail.com>
2015-10-16 23:37:59 +1000

    Fix buoy create error

--- server/models/buoy.go
+++ server/models/buoy.go
@@ -57,12 +57,12 @@ func (db *DB) GetBuoyById(id int) (*Buoy, error) {
 
 // Insert a new Buoy into the database.
 func (db *DB) CreateBuoy(buoy *Buoy) (int64, error) {
-	query, err := db.Preparex("INSERT INTO buoy (guid) VALUES(?);")
+	query, err := db.Preparex("INSERT INTO buoy (guid, active_buoy_instance_id, archived) VALUES(?, ?, ?);")
 	if err != nil {
 		return -1, err
 	}
 
-	result, err := query.Exec(buoy.Guid)
+	result, err := query.Exec(buoy.Guid, 0, 0)
 	if err != nil {
 		return -1, err
 	}

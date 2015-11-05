35533ce62c09ca96c739b27ffc0966eb98827649
Andrew Dyer <andrew@dyergroup.com.au>
2015-11-06 00:31:07 +1000

    Hello route tests

---
+++ web_app/app/core/core.routes.spec.js
@@ -0,0 +1,40 @@
+/**
+ * Flood Monitoring System
+ * Version 0.0.1 (Duyung)
+ *
+ * Copyright (C) Team Neptune
+ * All rights reserved.
+ *
+ * @author     Andrew Dyer <andrew@dyergroup.com.au>
+ * @version    0.0.1
+ * @copyright  Team Neptune (2015)
+ * @link       https://github.com/aclel/deco3801
+ */
+(function() {
+    'use strict';
+    
+    /** Unit tests for core routes */
+    describe('Routes: core', function() {
+        var state;
+
+        beforeEach(module('app'));
+
+        beforeEach(inject(function($state) {
+            state = $state;
+        }));
+
+        describe('Hello route', function() {
+            it('should be defined', function() {
+                var expected = {
+                    name: 'hello',
+                    url: '/hello',
+                    controller: 'HelloController',
+                    data: {
+                        access: 'any'
+                    }
+                };
+                expect(angular.equals(state.get('hello'), expected)).toBe(true);
+            });
+        });
+    });
+})();
